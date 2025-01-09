c1b76330804cce68bd68932c49bee4190d2611f9
ginares <ginaresfrancine@gmail.com>
2025-01-10 01:58:31 +0800

    added a connection timeout

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    connectTimeout: 20000 // 20 seconds
 
 })
 db.connect((error) =>{
